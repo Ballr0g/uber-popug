4c5548f7092380941e5655d571439beb6e5c709f
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-03 14:00:37 +0300

    POPUG-002 Updated TaskTrackerController to utilize the created service structure.

--- task-tracker/src/main/java/org/uber/popug/task/tracker/controller/TaskTrackerController.java
+++ task-tracker/src/main/java/org/uber/popug/task/tracker/controller/TaskTrackerController.java
@@ -1,16 +1,20 @@
 package org.uber.popug.task.tracker.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.uber.popug.task.tracker.exception.NotImplementedException;
 import org.uber.popug.task.tracker.rest.generated.api.TasksApi;
 import org.uber.popug.task.tracker.rest.generated.model.PostTasksRequestDto;
 import org.uber.popug.task.tracker.rest.generated.model.PostTasksResponseDto;
+import org.uber.popug.task.tracker.service.TaskTrackerService;
 
 @RestController
+@RequiredArgsConstructor
 public class TaskTrackerController implements TasksApi {
+    private final TaskTrackerService taskTrackerService;
     @Override
     public ResponseEntity<PostTasksResponseDto> postTasks(PostTasksRequestDto postTasksRequestDto) {
-        throw new NotImplementedException(TaskTrackerController.class, "postTasks");
+        return new ResponseEntity<>(taskTrackerService.addNewTask(postTasksRequestDto), HttpStatus.CREATED);
     }
 }
