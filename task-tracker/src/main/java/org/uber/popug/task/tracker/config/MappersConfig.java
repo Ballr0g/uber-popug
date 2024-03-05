439bacbb2ee07de126e11cb792dd6983a2f3e95e
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-05 21:28:31 +0300

    POPUG-004 Updated mappers with correct mappings.

--- task-tracker/src/main/java/org/uber/popug/task/tracker/config/MappersConfig.java
+++ task-tracker/src/main/java/org/uber/popug/task/tracker/config/MappersConfig.java
@@ -4,6 +4,7 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.uber.popug.task.tracker.mapping.TasksDtoMapper;
+import org.uber.popug.task.tracker.mapping.UsersPersistenceMapper;
 
 @Configuration(proxyBeanMethods = false)
 public class MappersConfig {
@@ -11,6 +12,11 @@ public class MappersConfig {
     @Bean
     public TasksDtoMapper tasksRequestDtoMapper() {
         return Mappers.getMapper(TasksDtoMapper.class);
+    }
+
+    @Bean
+    public UsersPersistenceMapper usersPersistenceMapper() {
+        return Mappers.getMapper(UsersPersistenceMapper.class);
     }
 
 }
