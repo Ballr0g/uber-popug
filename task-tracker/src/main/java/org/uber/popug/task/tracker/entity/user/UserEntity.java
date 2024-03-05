064a8052afb6ea86b60c507decac959595048c19
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-05 21:20:37 +0300

    POPUG-004 Finalized persistence entities structure based on records.

--- task-tracker/src/main/java/org/uber/popug/task/tracker/entity/user/UserEntity.java
+++ task-tracker/src/main/java/org/uber/popug/task/tracker/entity/user/UserEntity.java
@@ -1,17 +1,15 @@
 package org.uber.popug.task.tracker.entity.user;
 
 import jakarta.annotation.Nonnull;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.uber.popug.task.tracker.entity.userrole.UserRoleEntity;
 
 import java.util.Set;
 import java.util.UUID;
 
-@Getter
-@RequiredArgsConstructor
-public class UserEntity {
-    @Nonnull private final UUID extUserId;
-    @Nonnull private final String login;
-    @Nonnull Set<UserRoleEntity> roles;
+public record UserEntity(
+        long userId,
+        @Nonnull UUID extPublicUserId,
+        @Nonnull String login,
+        @Nonnull Set<UserRoleEntity> roles
+) {
 }
