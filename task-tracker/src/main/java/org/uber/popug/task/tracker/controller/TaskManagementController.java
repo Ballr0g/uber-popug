4715a363e22f487550d5baad03e519d726edb915
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-06 23:04:34 +0300

    POPUG-006 Rename: TaskTrackerController -> TaskManagementController, added reassignment method.

--- task-tracker/src/main/java/org/uber/popug/task/tracker/controller/TaskManagementController.java
+++ task-tracker/src/main/java/org/uber/popug/task/tracker/controller/TaskManagementController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.uber.popug.task.tracker.exception.NotImplementedException;
 import org.uber.popug.task.tracker.rest.generated.api.TasksApi;
 import org.uber.popug.task.tracker.rest.generated.model.PostTasksRequestDto;
 import org.uber.popug.task.tracker.rest.generated.model.PostTasksResponseDto;
@@ -11,10 +12,15 @@ import org.uber.popug.task.tracker.service.TaskAddingService;
 
 @RestController
 @RequiredArgsConstructor
-public class TaskTrackerController implements TasksApi {
+public class TaskManagementController implements TasksApi {
     private final TaskAddingService taskAddingService;
     @Override
     public ResponseEntity<PostTasksResponseDto> postTasks(PostTasksRequestDto postTasksRequestDto) {
         return new ResponseEntity<>(taskAddingService.addNewTask(postTasksRequestDto), HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<Void> reassignAllTasks() {
+        throw new NotImplementedException(TaskManagementController.class, "reassignAllTasks");
     }
 }
