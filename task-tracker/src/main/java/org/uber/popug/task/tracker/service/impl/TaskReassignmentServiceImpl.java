b290407886cf78509d8180b085993ee8a2bf876c
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-06 23:16:43 +0300

    POPUG-007 Added a template for the TaskReassignmentService.

---
+++ task-tracker/src/main/java/org/uber/popug/task/tracker/service/impl/TaskReassignmentServiceImpl.java
@@ -0,0 +1,12 @@
+package org.uber.popug.task.tracker.service.impl;
+
+import org.uber.popug.task.tracker.service.TaskReassignmentService;
+
+public class TaskReassignmentServiceImpl implements TaskReassignmentService {
+
+    @Override
+    public void reassignAllTasks() {
+
+    }
+
+}
