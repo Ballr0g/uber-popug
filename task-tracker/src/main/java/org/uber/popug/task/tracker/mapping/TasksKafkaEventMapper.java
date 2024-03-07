51ad8af1eb268fa958eb9b2fe98de99bd2a25e1e
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-07 19:23:17 +0300

    POPUG-009 Updated TasksKafkaEventMapper tp support mapping to TaskReassignedEvent.

--- task-tracker/src/main/java/org/uber/popug/task/tracker/mapping/TasksKafkaEventMapper.java
+++ task-tracker/src/main/java/org/uber/popug/task/tracker/mapping/TasksKafkaEventMapper.java
@@ -3,7 +3,10 @@ package org.uber.popug.task.tracker.mapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.uber.popug.task.tracker.domain.task.Task;
+import org.uber.popug.task.tracker.entity.task.TaskEntity;
+import org.uber.popug.task.tracker.entity.user.UserEntity;
 import org.uber.popug.task.tracker.kafka.producer.dto.TaskCreatedEvent;
+import org.uber.popug.task.tracker.kafka.producer.dto.TaskReassignedEvent;
 
 @Mapper
 public interface TasksKafkaEventMapper {
@@ -16,5 +19,15 @@ public interface TasksKafkaEventMapper {
             expression = "java(java.time.ZonedDateTime.now(java.time.ZoneId.of(\"UTC\")).toLocalDateTime())"
     )
     TaskCreatedEvent toTaskCreatedEventFromBusiness(Task task);
+
+    @Mapping(source = "task.publicTaskId", target = "publicId")
+    @Mapping(source = "previousAssignee.extPublicUserId", target = "previousAssigneePublicId")
+    @Mapping(source = "newAssignee.extPublicUserId", target = "newAssigneePublicId")
+    @Mapping(source = "task.description", target = "description")
+    @Mapping(
+            target = "reassignmentDate",
+            expression = "java(java.time.ZonedDateTime.now(java.time.ZoneId.of(\"UTC\")).toLocalDateTime())"
+    )
+    TaskReassignedEvent toTaskReassignedEventFromBusiness(TaskEntity task, UserEntity previousAssignee, UserEntity newAssignee);
 
 }
