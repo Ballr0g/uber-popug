ecab407e8f7fd7b3a521beb065d73a03286f2b08
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-07 19:22:30 +0300

    POPUG-009 Added a method for sending reassign task events.

--- task-tracker/src/main/java/org/uber/popug/task/tracker/kafka/producer/TasksCUDEventProducer.java
+++ task-tracker/src/main/java/org/uber/popug/task/tracker/kafka/producer/TasksCUDEventProducer.java
@@ -1,9 +1,14 @@
 package org.uber.popug.task.tracker.kafka.producer;
 
 import org.uber.popug.task.tracker.domain.task.Task;
+import org.uber.popug.task.tracker.kafka.producer.dto.TaskReassignedEvent;
+
+import java.util.List;
 
 public interface TasksCUDEventProducer {
 
     void sendTaskCreationEvent(Task task);
+
+    void sendTaskReassignmentEvents(List<TaskReassignedEvent> taskReassignedEvents);
 
 }
--- task-tracker/src/main/java/org/uber/popug/task/tracker/kafka/producer/impl/KafkaTemplateTasksCUDEventProducer.java
+++ task-tracker/src/main/java/org/uber/popug/task/tracker/kafka/producer/impl/KafkaTemplateTasksCUDEventProducer.java
@@ -3,9 +3,13 @@ package org.uber.popug.task.tracker.kafka.producer.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import org.uber.popug.task.tracker.domain.task.Task;
 import org.uber.popug.task.tracker.kafka.producer.TasksCUDEventProducer;
+import org.uber.popug.task.tracker.kafka.producer.dto.TaskReassignedEvent;
 import org.uber.popug.task.tracker.mapping.TasksKafkaEventMapper;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class KafkaTemplateTasksCUDEventProducer implements TasksCUDEventProducer {
@@ -22,6 +26,14 @@ public class KafkaTemplateTasksCUDEventProducer implements TasksCUDEventProducer
         final var kafkaTaskProducerRecord = kafkaTaskMessage.asProducerRecord(tasksLifecycleKafkaTopicName);
 
         kafkaTemplate.send(kafkaTaskProducerRecord);
+    }
+
+    @Override
+    @Transactional
+    public void sendTaskReassignmentEvents(List<TaskReassignedEvent> taskReassignedEvents) {
+        taskReassignedEvents.stream()
+                .map(task -> task.asProducerRecord(tasksLifecycleKafkaTopicName))
+                .forEach(kafkaTemplate::send);
     }
 
 }
