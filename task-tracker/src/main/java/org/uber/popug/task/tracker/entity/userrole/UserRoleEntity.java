cca10d9bdf78850f7a5e59fcb32662ad50cb966f
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-09 16:19:24 +0300

    POPUG-HW-02 Review: renamed UserRoleEntity.userRoleId -> id.

--- task-tracker/src/main/java/org/uber/popug/task/tracker/entity/userrole/UserRoleEntity.java
+++ task-tracker/src/main/java/org/uber/popug/task/tracker/entity/userrole/UserRoleEntity.java
@@ -1,12 +1,9 @@
 package org.uber.popug.task.tracker.entity.userrole;
 
 import jakarta.annotation.Nonnull;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
-public class UserRoleEntity {
-    private final long userRoleId;
-    @Nonnull private final String roleName;
+public record UserRoleEntity(
+        long id,
+        @Nonnull String roleName
+) {
 }
