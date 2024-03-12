1b28a6eef1afb6fb850b7288354764b587ba46a3
Daniil Sagalov <danielsagel@outlook.com>
2024-03-12 13:34:08 +0300

    Feature/POPUG-012 billing service setup (#15)
    * POPUG-012 employee-billing - initial project setup.
    
    * POPUG-012 employee-billing - added application.yml, docker-compose.yml with postgres and EmployeeBillingApplication.
    
    * POPUG-012 Remapped postgres container port to avoid collisions with task-tracker.
    
    * POPUG-012 Implemented the db model for task-billing.
    
    ---------
    
    Setup the billing service.

---
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/EmployeeBillingApplication.java
@@ -0,0 +1,13 @@
+package org.uber.popug.employee.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBillingApplication.class, args);
+    }
+
+}
