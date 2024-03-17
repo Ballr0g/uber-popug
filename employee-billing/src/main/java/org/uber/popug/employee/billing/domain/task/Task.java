76ff7fdf63b35111ed79462ac45728adc242222a
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-17 14:34:25 +0300

    POPUG-013 Added missing assigneeId to Task business type.

--- employee-billing/src/main/java/org/uber/popug/employee/billing/domain/task/Task.java
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/domain/task/Task.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 public record Task(
         long id,
         @Nonnull UUID extPublicId,
+        @Nonnull UUID assigneeId,
         @Nonnull String description,
         @Nonnull LocalDateTime creationDate,
         @Nonnull Status status,
@@ -27,6 +28,7 @@ public record Task(
         return new Task(
                 taskIdProvider.generateDbTaskId(),
                 taskInfo.id(),
+                taskInfo.assigneeId(),
                 taskInfo.description(),
                 taskInfo.creationDate(),
                 Status.OPEN,
