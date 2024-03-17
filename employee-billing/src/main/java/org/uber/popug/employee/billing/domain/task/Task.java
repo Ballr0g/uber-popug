44857277974e54c4efccc3cef922b44e910c4bb4
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-17 15:53:31 +0300

    POPUG-013 Removed assigneeId from core Task business type.

--- employee-billing/src/main/java/org/uber/popug/employee/billing/domain/task/Task.java
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/domain/task/Task.java
@@ -2,15 +2,12 @@ package org.uber.popug.employee.billing.domain.task;
 
 import jakarta.annotation.Nonnull;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record Task(
         long id,
         @Nonnull UUID extPublicId,
-        @Nonnull UUID assigneeId,
         @Nonnull String description,
-        @Nonnull LocalDateTime creationDate,
         @Nonnull Status status,
         long assignmentCost,
         long completionCost
@@ -28,9 +25,7 @@ public record Task(
         return new Task(
                 taskIdProvider.generateDbTaskId(),
                 taskInfo.id(),
-                taskInfo.assigneeId(),
                 taskInfo.description(),
-                taskInfo.creationDate(),
                 Status.OPEN,
                 taskCostsProvider.calculateAssignmentCost(),
                 taskCostsProvider.calculateCompletionCost()
