1bdb91f40a4fe31d549aa8af006f47046fa29d64
Daniil Sagalov <danielsagel@outlook.com>
2024-03-18 02:15:55 +0300

    POPUG-014 Added BillingOperationsPersistenceMapper for mapping BillingOperationFullData to BillingOperationEntity.

--- employee-billing/src/main/java/org/uber/popug/employee/billing/config/MappersConfig.java
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/config/MappersConfig.java
@@ -3,6 +3,7 @@ package org.uber.popug.employee.billing.config;
 import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.uber.popug.employee.billing.mapping.BillingOperationsPersistenceMapper;
 import org.uber.popug.employee.billing.mapping.TasksBusinessKafkaEventMapper;
 import org.uber.popug.employee.billing.mapping.TasksCUDKafkaEventMapper;
 import org.uber.popug.employee.billing.mapping.TasksPersistenceMapper;
@@ -12,11 +13,6 @@ import org.uber.popug.employee.billing.mapping.UsersPersistenceMapper;
 public class MappersConfig {
 
     @Bean
-    public TasksCUDKafkaEventMapper tasksCUDKafkaEventMapper() {
-        return Mappers.getMapper(TasksCUDKafkaEventMapper.class);
-    }
-
-    @Bean
     public UsersPersistenceMapper usersPersistenceMapper() {
         return Mappers.getMapper(UsersPersistenceMapper.class);
     }
@@ -24,6 +20,16 @@ public class MappersConfig {
     @Bean
     public TasksPersistenceMapper tasksPersistenceMapper() {
         return Mappers.getMapper(TasksPersistenceMapper.class);
+    }
+
+    @Bean
+    public BillingOperationsPersistenceMapper billingOperationsPersistenceMapper() {
+        return Mappers.getMapper(BillingOperationsPersistenceMapper.class);
+    }
+
+    @Bean
+    public TasksCUDKafkaEventMapper tasksCUDKafkaEventMapper() {
+        return Mappers.getMapper(TasksCUDKafkaEventMapper.class);
     }
 
     @Bean
---
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/mapping/BillingOperationsPersistenceMapper.java
@@ -0,0 +1,20 @@
+package org.uber.popug.employee.billing.mapping;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.uber.popug.employee.billing.domain.aggregates.BillingOperationFullData;
+import org.uber.popug.employee.billing.entity.billing.operation.BillingOperationEntity;
+
+@Mapper
+public interface BillingOperationsPersistenceMapper {
+
+    @Mapping(source = "data.id", target = "id")
+    @Mapping(source = "data.publicId", target = "publicId")
+    @Mapping(source = "ownerUser.id", target = "ownerUserId")
+    @Mapping(source = "data.description", target = "description")
+    @Mapping(source = "data.credit", target = "credit")
+    @Mapping(source = "data.debit", target = "debit")
+    @Mapping(source = "ownerCycle.id", target = "billingCycleId")
+    BillingOperationEntity fromBusiness(BillingOperationFullData billingOperation);
+
+}
