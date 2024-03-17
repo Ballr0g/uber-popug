282865d15e6c6263b6b48376ec4a58228fb40908
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-17 15:27:12 +0300

    POPUG-013 Implemented mapping from UserEntity to User.

--- employee-billing/src/main/java/org/uber/popug/employee/billing/config/MappersConfig.java
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/config/MappersConfig.java
@@ -4,6 +4,7 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.uber.popug.employee.billing.mapping.TasksCUDKafkaEventMapper;
+import org.uber.popug.employee.billing.mapping.UsersPersistenceMapper;
 
 @Configuration(proxyBeanMethods = false)
 public class MappersConfig {
@@ -11,6 +12,11 @@ public class MappersConfig {
     @Bean
     public TasksCUDKafkaEventMapper tasksCUDKafkaEventMapper() {
         return Mappers.getMapper(TasksCUDKafkaEventMapper.class);
+    }
+
+    @Bean
+    public UsersPersistenceMapper usersPersistenceMapper() {
+        return Mappers.getMapper(UsersPersistenceMapper.class);
     }
 
 }
