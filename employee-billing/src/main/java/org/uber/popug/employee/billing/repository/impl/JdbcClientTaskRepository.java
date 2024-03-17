a6e8a25feb55cc4e534b807beb7b5775eac71ce3
d.sagalov <d.sagalov@tinkoff.ru>
2024-03-17 16:17:25 +0300

    POPUG-013 Added insertion of a replicated task for TaskRepository.

--- employee-billing/src/main/java/org/uber/popug/employee/billing/repository/impl/JdbcClientTaskRepository.java
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/repository/impl/JdbcClientTaskRepository.java
@@ -2,6 +2,7 @@ package org.uber.popug.employee.billing.repository.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.uber.popug.employee.billing.entity.task.TaskEntity;
 import org.uber.popug.employee.billing.repository.TaskRepository;
 
 @RequiredArgsConstructor
@@ -12,6 +13,14 @@ public class JdbcClientTaskRepository implements TaskRepository {
             SELECT NEXTVAL('EMPLOYEE_BILLING.tasks_id_seq')
             """;
 
+    private static final String INSERT_BILLING_TASK_SQL = /* language=postgresql */
+            """
+            INSERT INTO EMPLOYEE_BILLING.TASKS
+                (id, ext_public_id, assignee_id, description, assignment_cost, completion_cost)
+            VALUES
+                (:id, :extPublicId, :assigneeId, :description, :assignmentCost, :completionCost)
+            """;
+
     private final JdbcClient jdbcClient;
 
     @Override
@@ -19,5 +28,17 @@ public class JdbcClientTaskRepository implements TaskRepository {
         return jdbcClient.sql(GENERATE_NEXT_TASK_ID_SQL)
                 .query(Long.class)
                 .single();
+    }
+
+    @Override
+    public int saveReplicated(TaskEntity task) {
+        return jdbcClient.sql(INSERT_BILLING_TASK_SQL)
+                .param("id", task.id())
+                .param("extPublicId", task.extPublicId())
+                .param("assigneeId", task.assigneeId())
+                .param("description", task.description())
+                .param("assignmentCost", task.assignmentCost())
+                .param("completionCost", task.completionCost())
+                .update();
     }
 }
