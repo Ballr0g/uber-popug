028393aea862818d670c534cbfc2f34cd8ecf6e7
Daniil Sagalov <danielsagel@outlook.com>
2024-03-18 12:58:58 +0300

    POPUG-014 Integrated TransactionalAccountingServiceImpl with BillingAccountManagementService for updating user balance.

--- employee-billing/src/main/java/org/uber/popug/employee/billing/service/impl/TransactionalAccountingServiceImpl.java
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/service/impl/TransactionalAccountingServiceImpl.java
@@ -2,7 +2,10 @@ package org.uber.popug.employee.billing.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+import org.uber.popug.employee.billing.domain.aggregates.PaymentDataWithUser;
 import org.uber.popug.employee.billing.domain.aggregates.TaskWithAssignee;
+import org.uber.popug.employee.billing.domain.billing.PaymentData;
+import org.uber.popug.employee.billing.service.BillingAccountManagementService;
 import org.uber.popug.employee.billing.service.BillingOperationLogService;
 import org.uber.popug.employee.billing.service.TransactionalAccountingService;
 
@@ -10,11 +13,18 @@ import org.uber.popug.employee.billing.service.TransactionalAccountingService;
 public class TransactionalAccountingServiceImpl implements TransactionalAccountingService {
 
     private final BillingOperationLogService billingOperationLogService;
+    private final BillingAccountManagementService billingAccountManagementService;
 
     @Override
     @Transactional
     public void billForNewlyAssignedTask(TaskWithAssignee taskWithAssignee) {
         billingOperationLogService.createBillingOperationLogEntry(taskWithAssignee);
+
+        final var paymentData = new PaymentDataWithUser(
+                PaymentData.newCreditData(taskWithAssignee.task().costs().assignmentCost()),
+                taskWithAssignee.assignee()
+        );
+        billingAccountManagementService.payUserForInitialAssignment(paymentData);
     }
 
 }
