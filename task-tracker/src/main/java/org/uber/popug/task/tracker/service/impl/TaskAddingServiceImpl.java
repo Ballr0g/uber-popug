fafa29df1f33b41a65df92d0799157e3f71dd1ee
Daniil Sagalov <danielsagel@outlook.com>
2024-03-19 14:42:51 +0300

    POPUG-014 [task-tracker] The worst temporary fix possible for CUD and business task creation event ordering - Thread.sleep.

--- task-tracker/src/main/java/org/uber/popug/task/tracker/service/impl/TaskAddingServiceImpl.java
+++ task-tracker/src/main/java/org/uber/popug/task/tracker/service/impl/TaskAddingServiceImpl.java
@@ -1,6 +1,7 @@
 package org.uber.popug.task.tracker.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.uber.popug.task.tracker.kafka.producer.TasksBusinessEventProducer;
 import org.uber.popug.task.tracker.kafka.producer.TasksCUDEventProducer;
 import org.uber.popug.task.tracker.mapping.TasksDtoMapper;
@@ -13,6 +14,8 @@ import org.uber.popug.task.tracker.service.TaskAddingService;
 @RequiredArgsConstructor
 public class TaskAddingServiceImpl implements TaskAddingService {
 
+    private static final long SIMULATION_TIMEOUT_MILLIS_TO_TEMPORARILY_SLOW_DOWN_BUSINESS_EVENT = 500L;
+
     private final TasksDtoMapper tasksDtoMapper;
     private final TaskAssignmentService taskAssignmentService;
     private final TaskRepository taskRepository;
@@ -20,12 +23,15 @@ public class TaskAddingServiceImpl implements TaskAddingService {
     private final TasksCUDEventProducer tasksCUDEventProducer;
 
     @Override
+    @SneakyThrows
     public PostAddTaskResponseDto addNewTask(PostAddTaskRequestDto postTasksRequestDto) {
         final var taskForCreation = tasksDtoMapper.postTaskRequestsDtoToBusiness(postTasksRequestDto);
         final var assignedTask = taskAssignmentService.assignNewTask(taskForCreation);
         taskRepository.add(assignedTask);
-        tasksBusinessEventProducer.sendTaskCreationEvent(assignedTask);
         tasksCUDEventProducer.sendTaskCreatedReplicationEvent(assignedTask);
+        // Todo: get rid of this monstrosity, currently solves receiving CUD before business in the worst possible way.
+        Thread.sleep(SIMULATION_TIMEOUT_MILLIS_TO_TEMPORARILY_SLOW_DOWN_BUSINESS_EVENT);
+        tasksBusinessEventProducer.sendTaskCreationEvent(assignedTask);
 
         return tasksDtoMapper.postTasksResponseDtoFromBusiness(assignedTask);
     }
