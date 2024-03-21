274d32621a68971610c70373ca6ac8f118e2b749
Daniil Sagalov <danielsagel@outlook.com>
2024-03-21 19:40:58 +0300

    POPUG-018 [employee-billing] Implemented BillingAccountRepository.resetPositiveBalance.

--- employee-billing/src/main/java/org/uber/popug/employee/billing/repository/BillingAccountRepository.java
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/repository/BillingAccountRepository.java
@@ -2,6 +2,7 @@ package org.uber.popug.employee.billing.repository;
 
 import org.uber.popug.employee.billing.entity.billing.account.BillingAccountEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BillingAccountRepository {
@@ -9,5 +10,7 @@ public interface BillingAccountRepository {
     Optional<BillingAccountEntity> subtractAccountBalanceByOwnerUser(long ownerUserId, long toSubtract);
 
     Optional<BillingAccountEntity> increaseAccountBalanceByOwnerUserId(long ownerUserId, long toAdd);
+
+    List<BillingAccountEntity> resetPositiveBalance();
 
 }
