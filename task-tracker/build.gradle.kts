b316e20e40bfedf3eb9e85cffd2b210a24f2b9b9
Daniil Sagalov <danielsagel@outlook.com>
2024-03-24 23:24:58 +0300

    POPUG-021 [task-tracker] Integrated json schema download into task-tracker service build if the schema files are not present, added code generation.

--- task-tracker/build.gradle.kts
+++ task-tracker/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     id("org.springframework.boot") version "3.2.3"
     id("org.openapi.generator") version "7.0.1"
+    id("de.undercouch.download") version "5.6.0"
+    id("org.jsonschema2pojo") version "1.2.1"
 }
 
 group = "org.uber.popug.task.tracker"
@@ -20,6 +22,17 @@ val postgresVersion: String by project
 val uuidCreatorVersion: String by project
 
 val openApiGeneratedPath: String = "${buildDir}/generated/openapi"
+val jsonSchemaDownloadPath: String = "${buildDir}/downloads/json/schema"
+val jsonSchemaGeneratedPath: String = "${buildDir}/generated/jsonSchema2Pojo/src/main/java"
+
+val schemaRegistryHost: String by project
+val schemaRegistryPath: String = "${schemaRegistryHost}/registry/%s/schemas/%s/%d"
+val necessaryJsonSchemas: Map<String, String> = mapOf(
+    schemaRegistryPath.format("cud", "task-created", 1) to "task-created-replication-event-v1.json",
+    schemaRegistryPath.format("business", "task-created", 1) to "task-created-event-v1.json",
+    schemaRegistryPath.format("business", "task-reassigned", 1) to "task-reassigned-event-v1.json",
+    schemaRegistryPath.format("business", "task-completed", 1) to "task-completed-event-v1.json",
+)
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -116,7 +129,50 @@ tasks {
     named<JavaCompile>("compileJava") {
         inputs.files(named("processResources"))
         dependsOn(
-            named("openApiGenerate")
+            named("openApiGenerate"),
+            named("generateJsonSchema2Pojo")
         )
+    }
+
+    register<de.undercouch.gradle.tasks.download.Download>("downloadMissingJsonSchemas") {
+        // Do not download the schemas that are already present.
+        val schemasToDownload: MutableSet<String> = mutableSetOf()
+        necessaryJsonSchemas.forEach {
+            if (!file("${jsonSchemaDownloadPath}/${it.value}").exists()) {
+                schemasToDownload.add(it.key)
+            }
+            else {
+                println("Skipping download of ${it.value} because it's already loaded.")
+            }
+        }
+
+        src(schemasToDownload)
+        overwrite(false)
+        dest(jsonSchemaDownloadPath)
+
+        if (schemasToDownload.size == 1) {
+            dest("${jsonSchemaDownloadPath}/${necessaryJsonSchemas[schemasToDownload.first()]}")
+        }
+        else if (schemasToDownload.size > 1) {
+            eachFile {
+                val sourceUrl = sourceURL.toString()
+                name = necessaryJsonSchemas[sourceUrl] ?: error("URL not found in the mapping: $sourceUrl")
+            }
+        }
+    }
+
+    named<org.jsonschema2pojo.gradle.GenerateJsonSchemaJavaTask>("generateJsonSchema2Pojo") {
+        dependsOn(
+            named("downloadMissingJsonSchemas")
+        )
+    }
+
+    jsonSchema2Pojo {
+        targetPackage = "${group}.kafka.generated.dto"
+        targetDirectory = file(jsonSchemaGeneratedPath)
+        sourceFiles = files(jsonSchemaDownloadPath)
+        includeAdditionalProperties = false
+        usePrimitives = true
+        useTitleAsClassname = true
     }
 }
