6a60da212ec9797880eaf2ec8528643e4633ab02
Daniil Sagalov <danielsagel@outlook.com>
2024-03-26 20:30:40 +0300

    POPUG-021 [employee-billing] Added updated Json deserialization-related exceptions.

---
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/exception/technical/JsonClassMappingImpossibleException.java
@@ -0,0 +1,27 @@
+package org.uber.popug.employee.billing.exception.technical;
+
+import com.fasterxml.jackson.core.TreeNode;
+import lombok.Getter;
+
+@Getter
+public class JsonClassMappingImpossibleException extends RuntimeException {
+
+    private static final String JSON_MAPPING_TO_TYPE_IMPOSSIBLE_MESSAGE_TEMPLATE
+            = "Could not deserialize JSON: %s to class %s.";
+
+    private final Class<?> failedClass;
+    private final String jsonString;
+
+    public JsonClassMappingImpossibleException(Class<?> failedClass, TreeNode jsonTreeRoot, Exception cause) {
+        super(JSON_MAPPING_TO_TYPE_IMPOSSIBLE_MESSAGE_TEMPLATE.formatted(
+                        jsonTreeRoot.toString(),
+                        failedClass.getTypeName()
+                ),
+                cause
+        );
+
+        this.failedClass = failedClass;
+        this.jsonString = jsonTreeRoot.toString();
+    }
+
+}
