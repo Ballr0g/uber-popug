7423c0128e9824827c3f301944acfca79c21047e
Daniil Sagalov <danielsagel@outlook.com>
2024-03-28 00:39:07 +0300

    POPUG-023 [employee-billing] Added support for TaskCreatedReplicationEventV2 for TaskLifecycleCUDEventListener.

--- employee-billing/src/main/java/org/uber/popug/employee/billing/kafka/consumer/listener/TaskLifecycleCUDEventListener.java
+++ employee-billing/src/main/java/org/uber/popug/employee/billing/kafka/consumer/listener/TaskLifecycleCUDEventListener.java
@@ -5,6 +5,7 @@ import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.uber.popug.employee.billing.kafka.generated.dto.TaskCreatedReplicationEventV1;
+import org.uber.popug.employee.billing.kafka.generated.dto.TaskCreatedReplicationEventV2;
 import org.uber.popug.employee.billing.service.TaskBillingReplicationService;
 
 @Service
@@ -22,6 +23,11 @@ public class TaskLifecycleCUDEventListener {
     public void handleTaskCreatedCUDEvent(TaskCreatedReplicationEventV1 taskCreatedReplicationEventV1) {
         // Todo: guarantee exactly-once semantics!
         taskBillingReplicationService.replicateTaskToBilling(taskCreatedReplicationEventV1);
+    }
+
+    @KafkaHandler
+    public void handleTaskCreatedCUDEvent(TaskCreatedReplicationEventV2 taskCreatedReplicationEventV2) {
+        taskBillingReplicationService.replicateTaskToBilling(taskCreatedReplicationEventV2);
     }
 
 }
